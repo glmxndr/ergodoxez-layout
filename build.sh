a4fcd55da4a9f9c8ae3cff28d8792c14a8862e43
glmxndr <glmxndr@gmail.com>
2018-06-11 20:02:51 +0200

    fix: build and flash working dir

--- build.sh
+++ build.sh
@@ -15,21 +15,18 @@ function runqmk() {
 
 if [[ $1 == 'clean' ]]; then
     # 0. Clean
+    pushd $QMK_DIR
     runqmk make $KEYBOARD:$KEYMAP:clean
-    rm -rf .build
-    git clean -xfd
+    popd
     git submodule foreach --recursive git clean -xfd
-    git checkout -- .
 else
     # 1. Copy my files into their appropriate locations inside QMK
     mkdir -p $QMK_DIR/keyboards/$KEYBOARD/keymaps/$KEYMAP
     cp -r $KEYMAP $QMK_DIR/keyboards/$KEYBOARD/keymaps/
 
     # 2. Make teensy build
-    cd $QMK_DIR
+    pushd $QMK_DIR
     runqmk make $KEYBOARD:$KEYMAP
-
-    # 3. make it accessible
-    cp -r .build ../.build
+    popd
 fi
 
--- flash.sh
+++ flash.sh
@@ -3,4 +3,7 @@ KEYBOARD=ergodox_ez
 KEYMAP=glmxndr
 QMK_DIR=qmk_firmware
 
+pushd $QMK_DIR
 teensy_loader_cli -w -v -mmcu=atmega32u4 .build/ergodox_ez_glmxndr.hex
+popd
+
